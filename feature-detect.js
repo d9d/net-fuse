9a3193faac1f2da3fee1fd2254e41bdf64eaa000
Mark Kahn <m@vouchr.com>
2015-08-22 00:51:28 +0800

    Syncing work

--- feature-detect.js
+++ feature-detect.js
@@ -1,8 +1,9 @@
 var err = 0;
 var proto;
 
-if (!global.WeakMap || !(proto=WeakMap.prototype).set || !proto.get || !proto.has) {
-  err=1, console.error('WeakMap is not supported in this environment');
+if (!global.WeakMap || !(proto = WeakMap.prototype).set || !proto.get || !proto.has) {
+  err = 1;
+  console.error('WeakMap is not supported in this environment');
 }
 
 if (err) {
